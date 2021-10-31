02e43844bf0aaf33f866874cb0c08af665b81132
Ilya Vishnikin <rarebloor@gmail.com>
2021-10-31 18:42:47 +0300

    Add modules and config

--- modules/basecut.pm
+++ modules/basecut.pm
@@ -4,20 +4,29 @@
 #
 #
 package Proto;
+use Proto::testPB;
 
 # optional: start_line, end_line;
 #  start_word, end_word works normaly if passbase sorted
 sub baseCut {
-    my($path_base, $start_line, $end_line, $start_word, $end_word) = @_; 
+    my ($path_base, $start_line, $end_line, $start_word, $end_word) = @_; 
     my $current_line = 1;
+    my 
 
-    open PASSBASE, "<", $path_base;
+    die "Uncorrect start or end lines.\n" if ($start_line <= $end_line);
+    die "Password database is not sorted.\n" if (defined ($start_line, $end_word) and &testPB == 1);
+
+    open PASSBASE, "<", $path_base or die "Can't open password base: $path_base\n";
     while (<PASSBASE>) {
         $current_line++;
         next if ($current_line != $start_line);
-        next if (substr $_, 0, length $start_word ne $start_word);
-        
-    }
-}
+        next if (defined $start_word and substr $_, 0, length $start_word ne $start_word);
 
-sub 
+        open WRITEFILE, ">>", "./".(substr $_, 0, length $start_word);
+        print WRITEFILE "$_\n";
+        close WRITEFILE;
+
+        return if (defined $end_word and substr $_, 0, length $end_word eq $end_word)
+    }
+    close PASSBASE
+}
---
+++ modules/wconfig.pm
@@ -0,0 +1,9 @@
+package Proto;
+
+sub getNameLen {
+    my $name_len;
+    open CONFIG, "<", "../config/passbase.conf";
+
+    close CONFIG;
+
+}
